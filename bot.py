255ee732d34774fc81a534b2ea53374a82dfe62e
Neill <neillreyes@yahoo.ca>
2021-05-06 20:29:41 -0600

    Shell for bot.

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+import discord
 from discord.ext import commands
+from discord.ext.commands import CommandNotFound, MissingPermissions
 
 # ------- COGS -------
 
@@ -9,4 +11,36 @@ COGS = [
 
 class Barkeep(commands.Bot):
     def __init__(self, prefix, description=None, testing=False, **options):
-        
+        super().__init__(prefix, help_command=help_command, description=description, **options)
+        self.state = "init"
+
+intents = discord.Intents(
+    guilds=True, members=True, messages=True, reactions=True
+)
+
+bot = Barkeep(prefix=get_prefix, description=desc, pm_help=True,
+    activity = discord.Game(name f'Finding next encounters... | {config.DEFAULT_PREFIX}help'),
+    allowed_mentions=discord.AllowedMentions.none(), intents=intents)
+
+@bot.event
+async def on_ready():
+    console.log(f'Logging in as {bot.user.name} {bot.user.id}\n')
+
+@bot.event
+async def on_resumed():
+    console.log('Resumed.')
+
+@bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.message.add_reaction(emoji='😔')
+            return
+
+        raise error
+
+for cog in COGS:
+    bot.load_extension(cog)
+
+if __name__ == '__main__':
+    bot.state = "run"
+    bot.run(token)
